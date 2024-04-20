4b0f6a5f3b87529553536cec6da249ea5ec23691
yujisato <yujisato.usk.jpn@gmail.com>
2024-04-20 14:50:36 +0200

    fix: deploy dynamic routes test

--- src/app/studio/[[...index]]/page.tsx
+++ src/app/studio/[[...index]]/page.tsx
@@ -2,7 +2,6 @@
 import { NextStudio } from "next-sanity/studio";
 import React from "react";
 import config from "../../../../sanity.config";
-import { GetServerSidePropsContext } from "next";
 
 const AdminPage = () => {
   return <NextStudio config={config} />;
@@ -14,10 +13,3 @@ export default AdminPage;
 // };
 
 // export default AdminPage;
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
-  // Perform any data fetching or computations needed for rendering the page
-  // If no specific data fetching is needed, you can simply return an empty props object
-  return { props: {} };
-};

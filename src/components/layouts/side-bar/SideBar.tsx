b90c2c20c934c61fcb2db9c1f1cdedb77e7533f5
yujisato <yujisato.usk.jpn@gmail.com>
2024-04-20 18:44:04 +0200

    fix(styling):

--- src/components/layouts/side-bar/SideBar.tsx
+++ src/components/layouts/side-bar/SideBar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const SideBar = () => {
   return (
     <div className="sidebar">
-      <div className="sticky-header">
+      <div className="sidebar-header">
         <div className="sidebar-header">
           <h2 className="sidebar-title">Pulizia 2.0</h2>
           <p className="sidebar-text">
